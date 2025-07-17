546f721a26a4075a1e27e8fbd95b6c9ffd3277ab
Lukas Adamek <lukas.adamek.work@gmail.com>
2025-07-17 11:02:34 +0200

    Final reformat

--- src/main/java/com/shipmonk/sortedlinkedlist/Node.java
+++ src/main/java/com/shipmonk/sortedlinkedlist/Node.java
@@ -1,28 +1,27 @@
 package com.shipmonk.sortedlinkedlist;
 
 public class Node<T> {
-	
+
 	private T value;
-    private Node<T> next;
+	private Node<T> next;
 
 	public Node(T value) {
-        this.value = value;
-        this.next = null;
-    }
+		this.value = value;
+		this.next = null;
+	}
 
 	public T getValue() {
 		return value;
 	}
 
-    public void setValue(T value) {
+	public void setValue(T value) {
 		this.value = value;
 	}
-
 
 	public Node<T> getNext() {
 		return next;
 	}
-	
+
 	public void setNext(Node<T> next) {
 		this.next = next;
 	}
